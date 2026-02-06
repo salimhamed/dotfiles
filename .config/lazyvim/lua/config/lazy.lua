48938e1083f3a2998a0d3af509503d4e2116ffa8
Salim Hamed <salimhamed@gmail.com>
2026-02-06 13:35:10 -0800

    update lazyvim

--- .config/lazyvim/lua/config/lazy.lua
+++ .config/lazyvim/lua/config/lazy.lua
@@ -30,8 +30,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.lang.docker" },
-    -- load lazyvim coding extras
-    { import = "lazyvim.plugins.extras.ai.copilot" },
     -- load lazyvim util extras
     { import = "lazyvim.plugins.extras.util.dot" },
     -- import/override with your plugins
