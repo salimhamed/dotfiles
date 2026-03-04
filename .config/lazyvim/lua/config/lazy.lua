88aa00e1d60b779427bf78bd9ef0f944c0456375
Salim Hamed <salimhamed@gmail.com>
2026-03-03 22:12:59 -0800

    clean comments

--- .config/lazyvim/lua/config/lazy.lua
+++ .config/lazyvim/lua/config/lazy.lua
@@ -34,15 +34,12 @@ require("lazy").setup({
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
-  }, -- automatically check for plugin updates
+  },
   performance = {
     rtp = {
       -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
