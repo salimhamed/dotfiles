caa5b5feec227464c96ea9bb2c3330517940d814
Salim Hamed <salimhamed@gmail.com>
2026-03-03 22:13:09 -0800

    clean comments

--- .config/lazyvim/lua/config/options.lua
+++ .config/lazyvim/lua/config/options.lua
@@ -9,10 +9,7 @@ vim.opt.secure = true
 vim.g.lazyvim_picker = "telescope"
 
 -- python options
--- LSP Server to use for Python.
--- Set to "basedpyright" to use basedpyright instead of pyright.
 vim.g.lazyvim_python_lsp = "basedpyright"
--- Set to "ruff_lsp" to use the old LSP implementation version.
 vim.g.lazyvim_python_ruff = "ruff"
 
 -- Enable clipboard over SSH via OSC 52
