00688e5a37881a1ec5fb10fcfb81b5d12ad943eb
Salim Hamed <salimhamed@gmail.com>
2026-04-21 09:55:04 -0700

    lazyvim: add oil.nvim keymaps under leader-f

--- .config/lazyvim/lua/plugins/oil.lua
+++ .config/lazyvim/lua/plugins/oil.lua
@@ -5,4 +5,8 @@ return {
   opts = {},
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   lazy = false,
+  keys = {
+    { "<leader>fo", "<cmd>Oil<cr>", desc = "Oil (Directory of Current File)" },
+    { "<leader>fO", "<cmd>Oil .<cr>", desc = "Oil (cwd)" },
+  },
 }
