7f5f695d470809c9c02a2d8897016d4fa5735432
Salim Hamed <salimhamed@gmail.com>
2026-05-11 16:13:32 -0700

    Add [h/]h hunk jumps and PageUp/PageDown scroll to diffview

--- .config/lazyvim/lua/plugins/diffview.lua
+++ .config/lazyvim/lua/plugins/diffview.lua
@@ -5,14 +5,35 @@ return {
     { "<leader>go", ":DiffviewOpen ", desc = "DiffviewOpen" },
     { "<leader>gq", "<cmd>DiffviewClose<cr>", desc = "DiffviewClose" },
   },
-  opts = {
-    view = {
-      default = {
-        winbar_info = true, -- show rev/branch label in each split
+  opts = function()
+    local actions = require("diffview.actions")
+    return {
+      view = {
+        default = {
+          winbar_info = true, -- show rev/branch label in each split
+        },
+        file_history = {
+          winbar_info = true,
+        },
       },
-      file_history = {
-        winbar_info = true,
+      keymaps = {
+        view = {
+          { "n", "]h", "]c", { desc = "Jump to the next hunk" } },
+          { "n", "[h", "[c", { desc = "Jump to the previous hunk" } },
+        },
+        diff1_inline = {
+          { "n", "]h", actions.next_inline_hunk, { desc = "Jump to the next hunk" } },
+          { "n", "[h", actions.prev_inline_hunk, { desc = "Jump to the previous hunk" } },
+        },
+        file_panel = {
+          { "n", "<PageDown>", actions.scroll_view(0.25), { desc = "Scroll the code view down" } },
+          { "n", "<PageUp>", actions.scroll_view(-0.25), { desc = "Scroll the code view up" } },
+        },
+        file_history_panel = {
+          { "n", "<PageDown>", actions.scroll_view(0.25), { desc = "Scroll the code view down" } },
+          { "n", "<PageUp>", actions.scroll_view(-0.25), { desc = "Scroll the code view up" } },
+        },
       },
-    },
-  },
+    }
+  end,
 }
