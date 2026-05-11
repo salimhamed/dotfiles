1d81587797a7f2bcb7074732fc44206fc14ee1e8
Salim Hamed <salimhamed@gmail.com>
2026-05-11 16:18:36 -0700

    Add [h/]h hunk jumps from diffview file panel

--- .config/lazyvim/lua/plugins/diffview.lua
+++ .config/lazyvim/lua/plugins/diffview.lua
@@ -7,6 +7,20 @@ return {
   },
   opts = function()
     local actions = require("diffview.actions")
+
+    -- Jump to next/prev hunk in the adjacent diff window without leaving the panel.
+    local function jump_hunk_from_panel(motion)
+      return function()
+        local panel_win = vim.api.nvim_get_current_win()
+        vim.cmd("wincmd l")
+        if vim.api.nvim_get_current_win() == panel_win then
+          return
+        end
+        vim.cmd("normal! " .. motion .. "czz")
+        vim.api.nvim_set_current_win(panel_win)
+      end
+    end
+
     return {
       view = {
         default = {
@@ -28,10 +42,14 @@ return {
         file_panel = {
           { "n", "<PageDown>", actions.scroll_view(0.25), { desc = "Scroll the code view down" } },
           { "n", "<PageUp>", actions.scroll_view(-0.25), { desc = "Scroll the code view up" } },
+          { "n", "]h", jump_hunk_from_panel("]"), { desc = "Jump to the next hunk" } },
+          { "n", "[h", jump_hunk_from_panel("["), { desc = "Jump to the previous hunk" } },
         },
         file_history_panel = {
           { "n", "<PageDown>", actions.scroll_view(0.25), { desc = "Scroll the code view down" } },
           { "n", "<PageUp>", actions.scroll_view(-0.25), { desc = "Scroll the code view up" } },
+          { "n", "]h", jump_hunk_from_panel("]"), { desc = "Jump to the next hunk" } },
+          { "n", "[h", jump_hunk_from_panel("["), { desc = "Jump to the previous hunk" } },
         },
       },
     }
